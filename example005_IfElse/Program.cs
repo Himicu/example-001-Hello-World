0d4a2e2dc071a7e3312eb70db29ad5fedfda12b9
hizumi <nyatel@yandex.ru>
2023-04-19 12:52:11 +0400

    add example 005 If Else

---
+++ example005_IfElse/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+if(username.ToLower() == "маша"){ //.ToLower() переводит строку в нижний регистр 
+    Console.WriteLine("Ура это ты!");
+}
+else{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
